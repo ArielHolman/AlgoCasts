0720c3b9e3a28398b75428f003e76c4921e9328d
ArielHolman <ArielARodrigues01@gmail.com>
2021-01-18 13:51:28 -0600

    completed practice for fizz buzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,23 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++){
+    // multiple of 3 and 5
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log ('fizzbuzz')
+      // mulitple of 5
+    } else if (i % 5 === 0) {
+      console.log ("buzz")
+      }
+      // multiple of 3
+      else if (i % 3 === 0) {
+      console.log ("fizz")
+      } else {
+        // none of the above
+      console.log (i)
+      }
+  }
+}
 
 module.exports = fizzBuzz;
